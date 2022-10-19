c854551f4f2ba55ea9a684c80db2f7bfbc5b6951
congnghiahieu <21020540@vnu.edu.vn>
2022-10-19 13:07:38 +0700

    Web crawler

---
+++ src/routes/crawl.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const crawlController = require('../app/controllers/CrawlController');
+const path = require('path');
+
+router.get('/', crawlController.crawl);
+// dowload file
+router.get('/dowload/:file', function (req,res,next) {
+    const filePath = path.join(__dirname, `../public/crawl_data/${req.params.file}`);
+    res.download(`${filePath}`);
+})
+
+
+module.exports = router;
