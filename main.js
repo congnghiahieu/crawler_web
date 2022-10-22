575c119d77c74f513f897a3f0c7ff796aa137e91
congnghiahieu <21020540@vnu.edu.vn>
2022-10-22 17:05:53 +0700

    Fix main

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ const morgan = require('morgan');
 const express = require('express');
 const { engine } = require('express-handlebars');
 const app = express();
-const route = require('./routes/main');
+const route = require('./src/routes/main');
 const port = process.env.PORT || 5000;
 
 // Template engines
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.set('views', path.join(__dirname, 'src', 'resources', 'views'));
 
 // Use static files
 // __dirname chỉ tới: path_to_project/src
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 // Encodd UTF-8
 app.use(
